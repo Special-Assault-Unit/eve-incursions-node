d0a034506932757bbc3af95e93b6ab03f8f51c47
Lars Naurath <lars.naurath@gmail.com>
2022-10-23 12:25:38 +0200

    feat(communities): add multi tank/ language support

--- packages/frontend/src/pages/communities.tsx
+++ packages/frontend/src/pages/communities.tsx
@@ -15,46 +15,49 @@ export default function Home({activeCommunities}: ActiveCommunitiesQuery) {
   return (
     <div className={'communities'}>
       <h1>Incursion Communities</h1>
-      <table style={{width: "100%"}} className="table">
+      <table style={{width: '100%'}} className="table">
         <thead>
         <tr>
-          <th style={{width: "16px"}}>VG</th>
-          <th style={{width: "16px"}}>AS</th>
-          <th style={{width: "16px"}}>HQ</th>
+          <th style={{width: '16px'}}>VG</th>
+          <th style={{width: '16px'}}>AS</th>
+          <th style={{width: '16px'}}>HQ</th>
           <th className="hidden-xs">Tag</th>
           <th>Name</th>
-          <th style={{textAlign: "center"}} className="hidden-xs">Timezone</th>
-          <th style={{textAlign: "center"}}>Language</th>
-          <th style={{textAlign: "center"}}>Tank</th>
+          <th style={{textAlign: 'center'}} className="hidden-xs">Timezone</th>
+          <th style={{textAlign: 'center'}}>Language</th>
+          <th style={{textAlign: 'center'}}>Tank</th>
           <th>Channel</th>
         </tr>
-      </thead>
-      <tbody>
-      {activeCommunities.map((community) => {
-      
-      return (
-        <tr key={community.name}>
-          <td>
-            {community.vg && <img src={"/images/tick.png"} alt="VG"/>}
-          </td>
-          <td>
-            {community.as && <img src={"/images/tick.png"} alt="AS"/>}
-          </td>
-          <td>
-            {community.hq && <img src={"/images/tick.png"} alt="HQ"/>}
-          </td>
-          <td className="hidden-xs">{community.tag}</td>
-          <td>{community.name}</td>
-          <td style={{textAlign: "center"}} className="hidden-xs">{community.timezone}</td>
-          <td style={{textAlign: "center"}}><img src={`/images/languages/${community.language}.png`} alt={community.language} /></td>
-          <td style={{textAlign: "center"}}><img src={`/images/${community.tank}.png`} alt={community.tank}/></td>
-          <td>{community.channel}</td>
-        </tr>
-      );
-      })}
-      </tbody>
-    </table>
-</div>
-);
+        </thead>
+        <tbody>
+        {activeCommunities.map((community) => {
+          const languages = community.language.split(',');
+          const tanks = community.tank.split(',');
+
+          return (
+            <tr key={community.name}>
+              <td>
+                {community.vg && <img src={'/images/tick.png'} alt="VG"/>}
+              </td>
+              <td>
+                {community.as && <img src={'/images/tick.png'} alt="AS"/>}
+              </td>
+              <td>
+                {community.hq && <img src={'/images/tick.png'} alt="HQ"/>}
+              </td>
+              <td className="hidden-xs">{community.tag}</td>
+              <td>{community.name}</td>
+              <td style={{textAlign: 'center'}} className="hidden-xs">{community.timezone}</td>
+              <td style={{textAlign: 'center'}}>{languages.map(language => <img key={language} src={`/images/languages/${language}.png`} alt={language}
+                                                                                title={language}/>)}</td>
+              <td style={{textAlign: 'center'}}>{tanks.map(tank => <img key={tank} src={`/images/${tank}.png`} alt={tank} title={tank}/>)}</td>
+              <td>{community.channel}</td>
+            </tr>
+          );
+        })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
